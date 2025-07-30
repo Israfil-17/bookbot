5a1648ed238bd6da6da8c41349a4606c43713a4e
Israfil <112609953+Israfil-17@users.noreply.github.com>
2025-07-30 21:16:32 +0300

    refractor code in stats.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as text:
-        file_content = text.read()
-        return file_content
-
 def main():
-    words = get_book_text("books/frankenstein.txt").split()
-    word_count = len(words)
-    print(f"{word_count} words found in the document")
+    from stats import word_num
+    word_num()
     
 
 
